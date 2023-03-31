0bbc00e5096b7ba6bfc88350e6d92ed84729e9ff
Erasmo Antonio Zeledón Castro <111201331+Erasmo505@users.noreply.github.com>
2023-03-31 16:48:27 -0600

    Add files via upload

---
+++ Encritación MD5/Encritación MD5/Encriptacion_Md5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encritación_MD5
+{
+    internal class Encriptacion_Md5
+    {
+        public string Encriptar(string valor)
+        {
+            string Hash = "2k23";
+            byte[] data=UTF32Encoding.UTF8.GetBytes(valor);
+
+            MD5 md5 = MD5.Create();
+            TripleDES tripleDES = TripleDES.Create();
+
+            tripleDES.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(Hash));
+            tripleDES.Mode= CipherMode.ECB;
+
+            ICryptoTransform cryptoTransform = tripleDES.CreateEncryptor();
+            byte[] resultado= cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+
+            return Convert.ToBase64String(resultado) ;
+        }
+
+        public string Desencriptar(string valor)
+        {
+            string Hash = "2k23";
+            byte[] data = Convert.FromBase64String(valor);
+
+            MD5 md5 = MD5.Create();
+            TripleDES tripleDES = TripleDES.Create();
+
+            tripleDES.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(Hash));
+            tripleDES.Mode = CipherMode.ECB;
+
+            ICryptoTransform cryptoTransform = tripleDES.CreateDecryptor();
+            byte[] resultado = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+
+            return UTF8Encoding.UTF8.GetString(resultado);
+        }
+    }
+}
---
+++ Encritación MD5/Encritación MD5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encritación_MD5
+{
+    internal class Program
+    {
+        Encriptacion_Md5 md5= new Encriptacion_Md5();
+        static void Main(string[] args)
+        {
+            Encriptacion_Md5 md5 = new Encriptacion_Md5();
+            int resp;
+
+            do
+            {
+                Console.WriteLine("Menú...\n1.Encriptar.\n2.Desencriptar.\nDigite una opcion...");
+                int opcion = int.Parse(Console.ReadLine());
+                if (opcion < 1 || opcion >2)
+                {
+                    Console.WriteLine("Digite una opcion valida");
+                }
+                else if (opcion == 1)
+                {
+                    Console.WriteLine("***ENCRIPTACION***");
+                    Console.WriteLine("Inserte el texto a Encriptar:");
+                    string valor = Console.ReadLine();
+                    Console.WriteLine(md5.Encriptar(valor));
+                }
+                else if (opcion == 2)
+                {
+                    Console.WriteLine("***DESENCRIPTACION***");
+                    Console.WriteLine("Inserte el texto a Desencriptar:");
+                    string valor2 = Console.ReadLine();
+                    Console.WriteLine(md5.Desencriptar(valor2));
+                }
+                Console.WriteLine("¿Desea continuar?\n1.Si\n2.Salir");
+                resp=int.Parse(Console.ReadLine());
+            } while (resp==1);
+
+            Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
